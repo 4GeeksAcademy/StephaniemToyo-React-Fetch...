6f728bef6d2773e986bea18754d9ca76d5faa371
Stephaniemtoyo <162191678+Stephaniemtoyo@users.noreply.github.com>
2024-06-24 08:34:14 +0000

    ultimos cambios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,115 @@
-import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+import React, { useEffect, useState } from "react";
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [tareas, setTareas] = useState([]);
+    const [tarea, setTarea] = useState("");
+    const [showResetButton, setShowResetButton] = useState(false);
+    const fetchGetTasks = () => {
+        fetch('https://playground.4geeks.com/todo/users/StephanieMToyo', {
+            method: "GET",
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setTareas(data.todos);
+            })
+            .catch(error => console.log(error));
+    };
+    const fetchPostTasks = () => {
+        const nuevaTarea = {
+            label: tarea,
+            is_done: false
+        };
+        fetch('https://playground.4geeks.com/todo/todos/StephanieMToyo', {
+            method: "POST",
+            body: JSON.stringify(nuevaTarea),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                fetchGetTasks();
+                setTarea("");
+                setShowResetButton(true); /* mostramos el boton al postear*/
+            })
+            .catch(error => console.log(error));
+    };
+    const fetchDeleteTasks = (id) => {
+        fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+            method: "DELETE",
+        })
+            .then(response => console.log(response))
+            .then(data => {
+                fetchGetTasks();
+                console.log(data);
+            })
+            .catch(error => console.log(error));
+    };
+    const fetchDeleteUser = () => {
+        fetch('https://playground.4geeks.com/todo/users/StephanieMToyo', {
+            method: "DELETE",
+        })
+            .then(response => console.log(response))
+            .then(data => {
+                console.log("Usuario eliminado:", data);
+                fetchCreateUser();
+                setShowResetButton(false);   /* para esconder el boton*/
+            })
+            .catch(error => console.log(error));
+    };
+    const fetchCreateUser = () => {
+        fetch('https://playground.4geeks.com/todo/users/StephanieMToyo', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ todos: [] })
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log("Usuario creado:", data);
+                fetchGetTasks();
+            })
+            .catch(error => console.log(error));
+    };
+    useEffect(() => {
+        fetchGetTasks();
+    }, []);
+    const handleChange = (e) => {
+        setTarea(e.target.value);
+    };
+    const handleKeyEnter = (e) => {
+        if (e.code === "Enter" || e.code === "Intro") {
+            fetchPostTasks();
+        }
+    };
+    return (
+        <div className="hola5">
+            <h1>To do with React and Fetch</h1>
+            <input
+                type="text"
+                name="tarea"
+                id="tarea"
+                onChange={handleChange}
+                value={tarea}
+                onKeyDown={handleKeyEnter}
+            />
+            <ul className="list-group">
+                {tareas?.map((tareaItem, index) => (
+                    <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+                        {tareaItem.label}
+                        <button className="btn btn-danger" id="botonrojo" onClick={() => fetchDeleteTasks(tareaItem.id)}>Eliminar</button>
+                    </li>
+                ))}
+            </ul>
+            {showResetButton && (
+                <button className="btn btn-dark" onClick={fetchDeleteUser}>Reiniciar To Do</button>
+            )}
+        </div>
+    );
 };
-
 export default Home;
+
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,11 @@
 //import react into the bundle
-import React from "react";
-import ReactDOM from "react-dom/client";
-
+import React from "react"; // importacion desde react
+import ReactDOM from "react-dom/client"; //iimportacion de react dom
 // include your styles into the webpack bundle
 import "../styles/index.css";
-
 //import your own components
 import Home from "./component/home.jsx";
-
-//render your react application
+//iniciamos nuestra application
+//render es mostrar en pantalla
+//componente vamos a llamar a todo elemento de React que podamos ejecutar
 ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
-
